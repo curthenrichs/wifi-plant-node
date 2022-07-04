cb1c40500329621a90f58654457f3706f6e91e01
Curt Henrichs <curthenrichs@gmail.com>
2022-07-04 15:28:52 -0700

    initial project

---
+++ ESP8266_Plant_Sensor_Node.ino
@@ -0,0 +1,27 @@
+/**
+
+https://www.ti.com/lit/ds/symlink/cd74hc4051.pdf?HQS=dis-dk-null-digikeymode-dsf-pf-null-wwe&ts=1653256681850&ref_url=https%253A%252F%252Fwww.ti.com%252Fgeneral%252Fdocs%252Fsuppproductinfo.tsp%253FdistId%253D10%2526gotoUrl%253Dhttps%253A%252F%252Fwww.ti.com%252Flit%252Fgpn%252Fcd74hc4051
+
+https://www.analog.com/media/en/technical-documentation/data-sheets/OP282_482.pdf
+
+*/
+
+#include <WiFiManager.h>
+
+
+#define RESET_BTN_PIN       D5
+#define EXTRN_LED_PIN       D0
+#define ANALOG_PIN          A0
+#define AMUX_SLCT_0_PIN     D1
+#define AMUX_SLCT_1_PIN     D2
+#define AMUX_SLCT_2_PIN     D3
+
+
+void setup() {
+    Serial.begin(9600);
+
+}
+
+void loop() {
+
+}
